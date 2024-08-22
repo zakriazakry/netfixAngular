2de252e9f4195aca831507922d4967d8a366b4c4
zakriazakry <xxxzekoxxx57@gmail.com>
2024-08-22 14:25:35 +0200

    as

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { AuthRes } from '../../interfaces/AuthRes.interface';
+
+import { environment } from '../../../environments/environment.development';
+import { catchError, throwError } from 'rxjs';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,15 +21,29 @@ export class AuthService {
     /*
     Login Func
     */
-   login(username:string,password:string):AuthRes
+   login(email:string,password:string):AuthRes
    {
+    this.http.post(environment.baseUrl+"auth/login",{
+      "email" :email,
+      "password" : password
+    }).subscribe({
+      next(value) {
+          console.log(value);
+      },
+      error(err) {
+          console.log(err);
+      },
+    });
      return {
        errorMsg:null,
        status:true,
        token:"ooooooooooo"
       };
     }
-
+  private handleError(error: any) {
+    console.error('An error occurred:', error);
+    return throwError(error);
+  }
   }
 
   // import * as CryptoJS from 'crypto-js';
