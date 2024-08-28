54c68d4d52030c501d3c012b218f791fa3cc50c1
zakriazakry <xxxzekoxxx57@gmail.com>
2024-08-28 14:13:24 +0200

    as

--- src/app/components/cards/dashboardCard/dashboardCard.module.ts
+++ src/app/components/cards/dashboardCard/dashboardCard.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardCardComponent } from './dashboardCard.component';
+import { AnimatedCounterComponent } from "../../admin/counter/counter.component";
 
 @NgModule({
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    AnimatedCounterComponent
+],
   declarations: [DashboardCardComponent],
   exports:[
     DashboardCardComponent
--- src/app/pages/dashboard/movies/moviedetails.dh/moviedetails.dh.component.ts
+++ src/app/pages/dashboard/movies/moviedetails.dh/moviedetails.dh.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
+import {  MovieDatails } from '../../../../interfaces/MovieDatails.interface';
 import { Component } from '@angular/core';
+import { VideoRunnerPipe } from '../../../../pipes/video-runner.pipe';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from '../../../../services/movies/movies.service';
 
 @Component({
   selector: 'app-moviedetails.dh',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './moviedetails.dh.component.html',
   styleUrl: './moviedetails.dh.component.scss'
 })
 export class MoviedetailsDhComponent {
+  movie!:MovieDatails;
+  movieId:any;
+  isloading = false;
+  constructor(
+    private videoRunner: VideoRunnerPipe,
+    private route: ActivatedRoute,
+    private router: Router,
+    private movieService: MoviesService
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      this.movieId = params.get('id');
+      if (this.movieId) {
+        this.fetchMovieDetails(this.movieId);
+      }
+    });
+  }
 
+
+  private fetchMovieDetails(id: string): void {
+    this.isloading = true;
+    this.movieService.getMovieInfoById(id).subscribe({
+      next: (res) => {
+        console.log(res);
+        if (!res.info ||
+          !res.movie_data ||
+          res.tmdb === undefined) {
+          // this.router.navigate(["/"]);
+        }
+        this.movie = res;
+        this.isloading = false;
+        // this.playMainMovieUrl = this.getVideoUrl();
+      },
+      error: (err) => {
+        console.error('Error fetching movie details:', err);
+
+        this.isloading = false;
+      },
+    });
+  }
 }
